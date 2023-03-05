305fd429d6e91035f44777c363cc4cc7bcfe58c1
Valeria <valeriakuz92@mail.ru>
2023-03-05 19:52:40 +0500

    savet again

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу которая из имеющегося массива строк формирует массив из строк длина которых меньше, либо равна трём символам
+
+Console.WriteLine("Введите желаемую строку для обработки: ");
+
+string originalString = Console.ReadLine();
+string[] enteredArray = originalString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine("Исходный массив: ");
+
+for (int i = 0; i < enteredArray.Length; i++)
+    Console.Write(enteredArray[i] + " ");
+Console.WriteLine();
+
+Console.WriteLine("Отсортированный массив: ");
+
+var resultString = new string[enteredArray.Length];
+var size = 0;
+foreach (var value in enteredArray)
+{
+    if (value.Length <= 3)
+    {
+        resultString[size] = value;
+        Console.Write(resultString[size] + ' ');
+        size++;
+    }
+}
